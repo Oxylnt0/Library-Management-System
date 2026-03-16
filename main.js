3e2cd0adcee20e7e4014b62dddf27bda8c0898ef
Oxylnt0 <simounandre@gmail.com>
2026-03-17 00:52:08 +0800

    ui revisions

--- main.js
+++ main.js
@@ -20,8 +20,8 @@ function createWindow() {
 
     
     //mainWindow.loadFile('public_view/public_view.html');
-    mainWindow.loadFile('admin_html/admin_login.html');
-    //mainWindow.loadFile('admin_html/admin_dashboard.html');
+    //mainWindow.loadFile('admin_html/admin_login.html');
+    mainWindow.loadFile('admin_html/admin_dashboard.html');
 }
 
 app.whenReady().then(() => {
